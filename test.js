987e4d749f359f0c940620c74aad31abf3775520
廖轩 <liaoxuan@lezhixing.com.cn>
2019-05-28 22:44:08 +0800

    feat:添加new关键字

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
-let obj = {
-  name: 'salvatore',
-  id: 'adfhlw452345lahfa1234lha0per8f7',
-  type: 'new',
-  area: 'ss'
+
+// let str = '"8f2dd4ab9f0f4b89a1be68d2f2622b7b,aaf64e6d738b43aaa0ccd941afe9ec21,","",leadAudit,user_task001,"aaf64e6d738b43aaa0ccd941afe9ec21,"'
+// let p = str.replace(/"(.*?)"/g, match => {
+//   console.log('match---->', match)
+//   if(match){
+//     return match.replace(/,/g,';')
+//   }
+// })
+// console.log('--=-=-=-=-=>>>', p)
+// let arr = p.split(',')
+// console.log('========>', arr)
+
+let sa = {
+  name:'xuanliao',
+  age:25,
+  say: function () {
+    console.log(`我叫${this.name},我今年${this.age}`)
+  }
 }
-let path = ''
-Object.keys(obj).forEach(item => {
-  path = path ? path += `&${item}=${obj[item]}` : path += `/?${item}=${obj[item]}`
-})
-console.log(path)
+
+let salvatore = Object.create(sa)
+salvatore.age = 23
+salvatore.say()
