4c8fcc733284d3dffd66c8482c26dacf8f51ecaf
廖轩 <liaoxuan@lezhixing.com.cn>
2019-05-30 20:04:39 +0800

    feat添加this关键字的相关信息

--- test.js
+++ test.js
@@ -1,23 +1,7 @@
-
-// let str = '"8f2dd4ab9f0f4b89a1be68d2f2622b7b,aaf64e6d738b43aaa0ccd941afe9ec21,","",leadAudit,user_task001,"aaf64e6d738b43aaa0ccd941afe9ec21,"'
-// let p = str.replace(/"(.*?)"/g, match => {
-//   console.log('match---->', match)
-//   if(match){
-//     return match.replace(/,/g,';')
-//   }
-// })
-// console.log('--=-=-=-=-=>>>', p)
-// let arr = p.split(',')
-// console.log('========>', arr)
-
-let sa = {
-  name:'xuanliao',
-  age:25,
-  say: function () {
-    console.log(`我叫${this.name},我今年${this.age}`)
-  }
-}
-
-let salvatore = Object.create(sa)
-salvatore.age = 23
-salvatore.say()
+let push = Function.prototype.apply.bind(Array.prototype.push)
+// let push = Array.prototype.push.bind(Array)
+let arr = [1,2,43,4]
+let a = push(arr,[3])
+console.log(arr)
+arr.pop()
+console.log(arr)
