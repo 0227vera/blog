2aead6c4a3b95d3b011870cb7fb36c154cb0414b
廖轩 <liaoxuan@lezhixing.com.cn>
2019-06-27 14:29:07 +0800

    perf: 修改about

--- docs/.vuepress/components/myself.vue
+++ docs/.vuepress/components/myself.vue
@@ -7,12 +7,13 @@
       </li>
     </ul>
     <hr>
-    <ul>
-      <li v-for="(item,index) in skillInfo">
+    <ul class="skillInfo">
+      <li v-for="(item,index) in skillInfo" :key="index">
         <span>{{index+1}}.</span>
         <span>{{item}}</span>
       </li>
     </ul>
+    <hr>
   </div>
 </template>
 
@@ -29,10 +30,13 @@ export default {
         { name: "状态", value: "求职中" }
       ],
       skillInfo:[
-        '熟练掌握HTML5、CSS3、Javascript开发',
-        '熟练使用vue、vuex，有复杂应用场景开发经验',
+        '熟练掌握HTML5、CSS3、Javascript开发，熟练使用scss，less',
+        '能熟练使用第三方组件库，element-ui、vux、vant等',
+        '熟练使用vue、vue-cli2、vue-cli3、vue-router、vuex、axios，有复杂应用场景开发经验',
         '熟练使用ES6,对ES6相关的知识有一定的理解',
-        '熟悉nodeJS开发（正在学习）'
+        '熟练使用webpack,npm包管理工具',
+        '熟悉nodeJS开发（正在学习）',
+        '熟练使用git操作',
       ]
     };
   },
@@ -53,6 +57,22 @@ export default {
   .baseInfo {
     width: 960px;
     overflow: hidden;
+    position: relative;
+    &::after{
+      content: '基础信息';
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      font-size: 40px;
+      text-align: center;
+      line-height: 80px;
+      letter-spacing: 80px;
+      z-index:-1;
+      color: #eeeeee;
+      font-style: oblique;
+    }
     li {
       width: 320px;
       float: left;
@@ -65,9 +85,35 @@ export default {
       span:nth-child(1){
         display: inline-block;
         width: 80px;
-        text-align: right
+        text-align: right;
       }
     }
+  }
+  .skillInfo{
+    margin-top: 10px;
+    position: relative;
+    &::after{
+      content: '专业技能';
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 50%;
+      left: 0;
+      font-size: 40px;
+      text-align: center;
+      margin-top:-40px;
+      // line-height: 80px;
+      letter-spacing: 80px;
+      z-index:-1;
+      color: #cccccc;
+      font-style: oblique;
+    }
+    span:nth-child(1){
+      margin-right: 5px;
+    }
+  }
+  hr{
+    margin-top: 10px;
   }
 }
 </style>
--- docs/about/README.md
+++ docs/about/README.md
@@ -11,6 +11,4 @@ features:
 ---
 ---
 <myself />
-
-
  <back-to-top />
