160642e46cf7fcd29656fb38a566f1ef5227052f
廖轩 <liaoxuan@lezhixing.com.cn>
2019-08-26 15:15:11 +0800

    perf: 简历完善

--- docs/.vuepress/components/myself.vue
+++ docs/.vuepress/components/myself.vue
@@ -16,9 +16,30 @@
     <hr>
     <ul class="itemHisInfo">
       <li v-for="(item,index) in itemHisInfo" :key="index">
-        <span>{{item.name}}</span>
-        <span>{{item.time}}</span>
-        <span>{{item.role}}</span>
+        <div>
+          <span>项目名称：</span>
+          <span>{{item.name}}</span>
+        </div>
+        <div>
+          <span>项目开发时间：</span>
+          <span>{{item.time}}</span>
+        </div>
+        <div>
+          <span>项目中的角色：</span>
+          <span>{{item.role}}</span>
+        </div>
+        <div>
+          <span>是否前后端分离：</span>
+          <span>{{item.isSeparate ? '是' : '否'}}</span>
+        </div>
+        <div>
+          <span>项目内容和职责：</span>
+          <ul>
+            <li v-for="(du,duIndex) in item.duty" :key="duIndex">
+              <span>{{duIndex+1}}.{{du}}</span>
+            </li>
+          </ul>
+        </div>
       </li>
     </ul>
   </div>
@@ -49,34 +70,83 @@ export default {
       ],
       itemHisInfo:[
         {
-          name:'公文流转移动端', 
+          name:'多级公文流转移动端', 
           time:'201906-201907',
-          role:'从0->1'
+          role:'从0->1',
+          isSeparate:true,
+          duty:[
+            'vue2+vue-cli3+vant项目构建和研发',
+            '表单设计器的预览和交互兼容H5',
+            '优化并制定相关的交互和动画标准'
+          ]
         },
-        { 
-          name:'公文流转web端', 
+        {
+          name:'多级公文流转web端', 
           time:'201903-201906',
-          role:'从0->1'
+          role:'从0->1',
+          isSeparate:true,
+          duty:[
+            'vue2+vue-cli2+elementUI构建和研发',
+            '公文流程设计器的单独生成',
+            '公文表单设计器的单独生成',
+            '使用canvas完成批注功能'
+          ]
         },
-        { 
+        {
           name:'内控系统', 
           time:'201901-201903',
-          role:'从0->1'
+          role:'从0->1',
+          isSeparate:false,
+          duty:[
+            '很老的办公应用项目的更新修改，前后端未分离',
+            '使用cdn的方式，vue+elementUI+jq的形式畸形研发',
+            '为什么没有使用前后端分离？因为产品版本需求不一样，每次修改的页面是部分而非一次全部迭代'
+          ]
         },
-        { 
+        {
           name:'工作流系统移动端', 
           time:'201811-201812',
-          role:'从0->1'
+          role:'从0->1',
+          isSeparate:true,
+          duty:[
+            '移动端工作流系统，使用vue2+vue-cli+vux研发',
+            '从0->1搭建',
+            '使用nodeJS+webpack自动生成相关需要的补丁文件'
+          ]
         },
         { 
           name:'工作流系统web端', 
           time:'201808-201811',
-          role:'维护者'
+          role:'维护者',
+          isSeparate:false,
+          duty:[
+            '很老的办公应用项目的更新修改，前后端未分离',
+            '使用cdn的方式，vue+elementUI+jq的形式畸形研发'
+          ]
         },
-        { 
+        {
           name:'工作流引擎和表单设计器维护', 
           time:'20180705-201808',
-          role:'维护者'
+          role:'维护者',
+          isSeparate:false,
+          duty:[
+            '基于开源项目bpmn-js经行修改的工作引擎，github地址：https://github.com/bpmn-io/bpmn-js',
+            '流程设计器ui上面主要使用的jq',
+            '使用preact（react的一个子类）书写的表单设计器插件，高度自定义，'
+          ]
+        },
+        { 
+          name:'小学科学课件开发', 
+          time:'2017011-201807',
+          role:'维护者',
+          isSeparate:false,
+          duty:[
+            '主要负责js编程，高度还原设计flash设计以及动画还原',
+            '熟悉并使用PIXI，使用框架jQuery-UI进行页面渲染',
+            '使用BabylonJS进行3D实验设计和实现',
+            '封装高复用，可维护性的组件',
+            '使用nodeJS，git，webpack，对项目进行管理',
+          ]
         },
       ]
     };
@@ -170,8 +240,48 @@ export default {
       margin-top:-40px;
       letter-spacing: 80px;
       z-index:-1;
-      color: #cccccc;
+      color: #aaa;
       font-style: oblique;
+    }
+    &>li{
+      width: 100%;
+      height: auto;
+      overflow: hidden;
+      margin-bottom: 20px;
+      padding-bottom: 20px;
+      position: relative;
+      &::after{
+        content: "";
+        width: 100%;
+        border-bottom: dashed 1px #000;
+        bottom: 0;
+        left: 0;
+        position: absolute;
+      }
+      div{
+        width: 50%;
+        float: left;
+        line-height: 40px;
+        &>span{
+          display: inline-block;
+          &:first-child{
+            width: 130px;
+            text-align: right;
+            vertical-align: middle;
+          }
+          &:last-child{
+            font-weight: 600;
+          }
+        }
+        &:last-child{
+          width: 100%;
+          ul{
+            display: inline-block;
+            vertical-align: middle;
+            font-weight: 600;
+          }
+        }
+      }
     }
   }
   hr{
