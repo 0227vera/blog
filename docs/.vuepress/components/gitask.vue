1350b31843c4d177565c4e9f9f2b145f12deaf37
廖轩 <liaoxuan@lezhixing.com.cn>
2019-10-28 09:42:32 +0800

    perf: 修改issues

--- docs/.vuepress/components/gitask.vue
+++ docs/.vuepress/components/gitask.vue
@@ -15,13 +15,14 @@ export default {
     }
   },
   mounted () {
+    // issues的长度在50字以内，如果超过就会显示`Error: Validation Failed`
     const gitalk = new Gitalk({
       clientID: '04d6cd9fea245bed44dd',
       clientSecret: '678d5db4c2e5db3200238b18fcc787f4e0bac398',
       repo: 'blog',
       owner: '0227vera',
       admin: ['0227vera'],
-      id: location.pathname,      // Ensure uniqueness and length less than 50
+      id: decodeURI(location.pathname),      // Ensure uniqueness and length less than 50
       distractionFreeMode: false  // Facebook-like distraction free mode
     })
     gitalk.render('gitalk-container')
