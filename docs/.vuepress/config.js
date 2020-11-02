40ecfb704eeea41f058b4a55d7d824b2f7c76286
liaoxuan <liaoxuan@sougo-inc.com>
2020-11-02 11:59:35 +0800

    perf:

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -35,7 +35,7 @@ module.exports = {
     ],
     sidebarDepth: 0,
     sidebar: {
-      "/essay/": ["/essay/", "20200925", "20201015"],
+      "/essay/": ["/essay/", "20200925", "20201015", "20201102"],
       "/posts/": [
         {
           title: "HTML",
