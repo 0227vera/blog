28dc26850b2e805817ad52af99304cd50a3400a0
salvatoreliaoxuan <salvatoreliaoxuan@didiglobal.com>
2021-09-23 16:52:58 +0800

    perf: 简历优化

--- docs/.vuepress/public/json/skillInfo.js
+++ docs/.vuepress/public/json/skillInfo.js
@@ -1,10 +1,9 @@
 export default [
-	'熟练掌握HTML5、CSS3、Javascript开发；熟练使用scss、less',
-	'能熟练使用第三方组件库，element-ui、vux、vant等',
-	'熟练使用vue、vue-cli2、vue-cli3、vue-router、vuex、axios；有复杂应用场景开发经验',
+	'熟练掌握HTML5、CSS3、Javascript、ES6开发；熟练使用scss、less',
+	'能熟练使用第三方组件库，element-ui、vant、echarts等',
+	'熟练使用vue2.0开发；有复杂应用场景开发经验',
 	'使用react、hooks、mobx、styled-components等开发模式',
-	'使用框架mpx开发小程序',
-	'熟练使用ES6，对ES6相关的知识有一定的理解',
+	'使用框架mpx开发小程序（百万级UV）',
 	'熟练使用webpack，npm包管理工具',
 	'preact、typescript、nodeJS开发维护经验',
 	'开源项目维护经验'
